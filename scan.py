a988dc7b8c411b0275f8f7156922f512e0c6f772
zLeki <85647342+zLeki@users.noreply.github.com>
2023-03-27 15:11:30 -0400

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,13 +1,16 @@
+#!/usr/bin/env python
+#!/usr/bin/env python
 import nmap
 
 def scanNetwork(network):
     return_list = []
     nm = nmap.PortScanner()
     a = nm.scan(hosts=network, arguments='-sn -T5')
-
+    print("scanning")
     for k, v in a['scan'].items():
         if str(v['status']['state']) == 'up':
             try:
+                print(str(v['addresses']['ipv4'])+" ← Found")
                 return_list.append([str(v['addresses']['ipv4']), str(v['addresses']['mac'])])
             except:
                 pass
